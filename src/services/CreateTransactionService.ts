601df7bd28bc890c90f7fffd1b3fbfec10134872
Romulo Braga <romulo.braga.92@gmail.com>
2020-04-19 20:43:16 -0300

    Desafio  até antes do delete

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    let categoryDB = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryDB) {
+      categoryDB = categoryRepository.create({ title: category });
+
+      await categoryRepository.save(categoryDB);
+    }
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError('insufficient funds', 400);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: categoryDB,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
